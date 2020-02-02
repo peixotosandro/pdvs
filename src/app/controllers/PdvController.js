50ac3f1170a03c5a6289831320ecececef98394a
Sandro Peixoto <sandro.peixoto@gmail.com>
2020-02-02 11:09:31 -0300

    Adicionado validação de campos.

--- src/app/controllers/PdvController.js
+++ src/app/controllers/PdvController.js
@@ -1,7 +1,20 @@
+import * as Yup from 'yup';
 import Pdv from '../models/Pdv';
 
 class PdvController {
   async store(request, response) {
+    const schema = Yup.object().shape({
+      tradingName: Yup.string().required(),
+      ownerName: Yup.string().required(),
+      document: Yup.string()
+        .matches(/[0-9]{13}\/[0-9]{4}/)
+        .required(),
+    });
+
+    if (!(await schema.isValid(request.body))) {
+      return response.status(400).json({ error: 'Validation error' });
+    }
+
     const { id, tradingName, ownerName, document } = await Pdv.create(
       request.body
     );
