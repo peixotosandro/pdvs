9f5b287541d6b69f5d742978700e6142dc00f283
Sandro Peixoto <sandro.peixoto@gmail.com>
2020-02-02 10:34:35 -0300

    Adicioando uuid para id dos registros.

--- src/app/controllers/PdvController.js
+++ src/app/controllers/PdvController.js
@@ -2,9 +2,11 @@ import Pdv from '../models/Pdv';
 
 class PdvController {
   async store(request, response) {
-    const { tradingName, ownerName, document } = await Pdv.create(request.body);
+    const { id, tradingName, ownerName, document } = await Pdv.create(
+      request.body
+    );
 
-    return response.json({ tradingName, ownerName, document });
+    return response.json({ id, tradingName, ownerName, document });
   }
 }
 
--- src/app/models/Pdv.js
+++ src/app/models/Pdv.js
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose';
+import uuid from 'node-uuid';
 
 const PdvSchema = new mongoose.Schema({
+  id: { type: String, default: uuid.v1 },
   tradingName: String,
   ownerName: String,
   document: String,
